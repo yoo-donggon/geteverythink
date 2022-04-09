b941881cfcb4050e85e2ab082fb97c7b5670f1a7
yoo-donggon <96458123+yoo-donggon@users.noreply.github.com>
2022-04-09 14:12:56 +0900

    css message popup

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+
+/*메시지 팝업*/
 function showPopup() { window.open("message.html", "a", "width=450, height=600, left=100, top=50"); }
 
 
-
+/*메시지 창*/
 $('.chat-input input').keyup(function(e) {
     if ($(this).val() == '')
       $(this).removeAttr('good');
