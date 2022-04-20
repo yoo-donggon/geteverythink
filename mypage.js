a4d3dd8649f2f5cf7e9dcb837b4aed238be061a2
yoo-donggon <96458123+yoo-donggon@users.noreply.github.com>
2022-04-20 12:09:01 +0900

    css message ellipsis

--- mypage.js
+++ mypage.js
@@ -1,6 +1,6 @@
 
 /*메시지 팝업*/
-function showPopup() { window.open("message.html", "a", "width=750, height=650, left=100, top=50"); }
+function showPopup() { window.open("message.html", "a", "width=1200, height=780, left=100, top=50"); }
 
 
 /*메시지 창*/
